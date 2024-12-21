fbaf1aa371f367ae260a163a18a03f971148e8ff
Shota Arakaki <syotaa1@gmail.com>
2024-12-22 02:10:36 +0900

    fix

--- src/utils.h
+++ src/utils.h
@@ -1,22 +1,26 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#if defined(_POSIX_C_SOURCE) && _POSIX_C_SOURCE >= 200809L
+#include <string.h>
+#define esml_strdup strdup
+#else
 #include <stdlib.h>
 #include <string.h>
-
 static inline char *esml_strdup(const char *str) {
   if (str == NULL) {
     return NULL;
   }
 
-  size_t len = strlen(str);
-  char *new_str = (char *)malloc(len + 1);
+  size_t len = strlen(str) + 1;
+  char *new_str = (char *)malloc(len);
   if (new_str == NULL) {
     return NULL;
   }
-  memcpy(new_str, str, len + 1);
+  memcpy(new_str, str, len);
 
   return new_str;
 }
+#endif
 
 #endif // UTILS_H
